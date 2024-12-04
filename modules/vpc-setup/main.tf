77c5d689ce8742ecc0375d2d92cc77df6f75ddfb
Lomax Osomba <Lomax.Osomba@students.ittralee.ie>
2024-12-04 14:42:58 +0000

    Fix VPC network name variable reference and correct routing mode spelling

--- modules/vpc-setup/main.tf
+++ modules/vpc-setup/main.tf
@@ -1,9 +1,9 @@
 #create network in a custom subnet mode
 resource "google_compute_network" "vpc_network" {
-  name                                      = "var.vpc_name"
+  name                                      = var.vpc_name
   description                               = "VPC network for the project"   
   auto_create_subnetworks                   = false
-  routing_mode                              = "GROBAL"
+  routing_mode                              = "GLOBAL"
 }
 
 resource "google_compute_subnetwork" "private_subnet" { 
